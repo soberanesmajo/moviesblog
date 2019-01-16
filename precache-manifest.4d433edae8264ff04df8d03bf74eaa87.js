46548ee330e3e4aacbb58e81ab4fe2a3a09533dc
soberanesmajo <soberanesmajo@gmail.com>
2019-01-16 00:44:53 -0600

    Updates

--- precache-manifest.4d433edae8264ff04df8d03bf74eaa87.js
+++ precache-manifest.4d433edae8264ff04df8d03bf74eaa87.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/moviesblog/static/js/runtime~main.82eefba0.js"
   },
   {
-    "revision": "80d02136748d913dbd00",
-    "url": "/moviesblog/static/js/main.80d02136.chunk.js"
+    "revision": "adbc136d4f7e2f7331eb",
+    "url": "/moviesblog/static/js/main.adbc136d.chunk.js"
   },
   {
     "revision": "4a34e45d34cafb7623bd",
     "url": "/moviesblog/static/js/1.4a34e45d.chunk.js"
   },
   {
-    "revision": "80d02136748d913dbd00",
+    "revision": "adbc136d4f7e2f7331eb",
     "url": "/moviesblog/static/css/main.2598e3af.chunk.css"
   },
   {
-    "revision": "f23bf415065ec15323111b8867fc0c55",
+    "revision": "e677e3b396e05483f13d0cf37b6bd658",
     "url": "/moviesblog/index.html"
   }
 ];
